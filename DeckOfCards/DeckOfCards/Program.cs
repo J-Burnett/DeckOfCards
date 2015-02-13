65d9675065c667a97d4b9b77a4a868f88bb3926f
Juli Anna Burnett <julianna.burnett@gmail.com>
2015-02-12 18:01:39 -0700

    Functioning Code and Passing all tests

--- DeckOfCards/DeckOfCards/Program.cs
+++ DeckOfCards/DeckOfCards/Program.cs
@@ -10,10 +10,11 @@ namespace DeckOfCards
     {
         static void Main(string[] args)
         {
-            
+            Deck myDeck = new Deck();
+            Console.ReadKey();
         }
-    }
 
+    }
 
     // When a new deck is created, you’ll create a card of each rank for each suit and add them to the deck of cards, 
     //      which in this case will be a List of Card objects.
@@ -28,17 +29,117 @@ namespace DeckOfCards
 	//     int CardsRemaining -- number of cards left in the deck
 	//     List<Card> DeckOfCards -- card waiting to be dealt
     //     List<Card> DiscardedCards -- cards that have been played
-    class Deck
-    {
+  
 
+    class Deck
+    {   
+        
+
+        //contains cards in a deck
+        public List<Card> DeckOfCards { get; set; }
+
+        //Contains card used
+        public List<Card> DiscardedCards { get; set; }
+
+
+        public Deck()
+        {
+            this.DeckOfCards = new List<Card>();
+            this.DiscardedCards = new List<Card>();
+                for(int i = 2; i < 15; i++)
+                {
+                    for(int j = 1; j < 5; j++)
+                    {
+                        this.DeckOfCards.Add(new Card((Suit)j, (Rank)i));
+                    }
+                }
+        }
+
+      
+
+        public List<Card> Deal(int numberOfCards)
+        {
+            List<Card> hand = new List<Card>();
+            for(int i = 0; i <numberOfCards; i++)
+            {
+                Console.WriteLine(DeckOfCards[i]);
+                hand.Add(DeckOfCards[i]);
+                DeckOfCards.Remove(DeckOfCards[i]);
+            }
+
+            return hand;
+        }
+
+        public void Discard(List<Card> hand)
+        {
+            for(int i = 0; i < hand.Count; i++)
+            {
+                DiscardedCards.Add(hand[i]); 
+            }
+        }
+
+        
+
+        public void Shuffle()
+        {
+            Random rng = new Random();
+
+            List<Card> shuffledDeck = new List<Card>();
+
+            while (DeckOfCards.Count > 0)
+            {
+                Card newCard = DeckOfCards[rng.Next(0, DeckOfCards.Count())];
+                shuffledDeck.Add(newCard);
+                DeckOfCards.Remove(newCard);
+            }
+
+            DeckOfCards = shuffledDeck;
+          
+        }
     }
 
     
     // What makes a card?
 	//     A card is comprised of it’s suit and its rank.  Both of which are enumerations.
     //     These enumerations should be "Suit" and "Rank"
+    
+    //Card Suits
+    public enum Suit
+    {
+        Spades = 1,
+        Diamonds,
+        Clubs,
+        Hearts
+    }
+
+    //Card Ranks
+    public enum Rank
+    {
+      
+        Two = 2,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace
+    }
+
     class Card
     {
+        public Suit CardSuit { get; set; }
+        public Rank CardRank { get; set; }
 
+        public Card (Suit suit, Rank rank)
+        {
+            this.CardSuit = suit;
+            this.CardRank = rank;
+        }
     }
 }
